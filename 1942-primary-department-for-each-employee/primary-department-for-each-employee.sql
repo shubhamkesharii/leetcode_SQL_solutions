72a8d8eb45ff8e0d43e6752eeefd87dbe557f8c1
Shubham Keshari <116225459+shubham-keshari1@users.noreply.github.com>
2024-03-21 09:02:09 -0400

    Time: 1375 ms (16.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,4 @@
+select employee_id, department_id
+from Employee
+where primary_flag = 'Y' or employee_id in (
+select employee_id from employee group by employee_id having count(department_id) = 1)
